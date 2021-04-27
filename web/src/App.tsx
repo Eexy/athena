39eaefbb756956336139babddf32b756162cad53
unknown <eexdhe@gmail.com>
2021-04-27 13:58:20 +0200

    start a new react app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,45 +1,26 @@
-import "./App.css";
-import "antd/dist/antd.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./pages/home";
-import { Navbar } from "./components/navbar";
-import { Login } from "./pages/login";
-import { Signup } from "./pages/signup";
-import { Dashboard } from "./pages/dashboard";
-import { useState } from "react";
-import { CookiesProvider } from "react-cookie";
-import { useCookies } from "react-cookie";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export const App = () => {
-  const [isAuth, setAuth] = useState(false);
-  const [cookies, setCookies, removeCookie] = useCookies(['jid']);
-
-  function getCookie(value: string){
-    document.cookie= 'jid='+value+"; path=/";
-  }
-
+function App() {
   return (
-    <CookiesProvider>
-      <Router>
-        <header style={{ boxShadow: "0 2px 8px #f0f1f2" }}>
-          <Navbar isAuth={isAuth} setAuth={setAuth} removeCookie={removeCookie}/>
-        </header>
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/login">
-            <Login setAuth={setAuth} setCookie={getCookie}/>
-          </Route>
-          <Route exact path="/signup">
-            <Signup setAuth={setAuth} setCookie={getCookie}/>
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard isAuth={isAuth} setAuth={setAuth} removeCookie={removeCookie}/>
-          </Route>
-        </Switch>
-      </Router>
-    </CookiesProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
+
+export default App;
