147dc74dcb6e633582f2ee3923b8e06764673ad9
unknown <eexdhe@gmail.com>
2021-05-07 13:58:09 +0200

    add event handler on menu

--- web/src/components/mobile-menu.tsx
+++ web/src/components/mobile-menu.tsx
@@ -2,17 +2,29 @@ import { Menu } from "antd";
 import { Link } from "react-router-dom";
 import { ExportOutlined, HomeOutlined } from "@ant-design/icons";
 import logout from "../utils/logout";
+import { useState } from "react";
 
 interface MobileMenuProps {}
 
 const MobileMenu: React.FC<MobileMenuProps> = () => {
+  const [current, setCurrent] = useState("dashboard");
+
   const handleLogoutClick = () => {
     logout();
   };
 
+  const handleMenuClick = (e: any) => {
+    setCurrent(e.key);
+  };
+
   return (
     <div className="mobile-menu" style={{ flex: 1 }}>
-      <Menu>
+      <Menu
+        style={{ border: "none" }}
+        onClick={handleMenuClick}
+        selectedKeys={[current]}
+        defaultSelectedKeys={[current]}
+      >
         <Menu.Item icon={<HomeOutlined />} key="dashboard">
           <Link to="/dashboard">Dashboard</Link>
         </Menu.Item>
--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,20 +1,28 @@
 import { ExportOutlined, HomeOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 import Title from "antd/lib/typography/Title";
+import { useState } from "react";
 import {Link} from "react-router-dom";
 import logout from "../utils/logout";
 
 const Sidebar = () => {
+  const [current, setCurrent] = useState("dashboard")
+
   const handleLogoutClick = () => {
     logout();
   };
 
+  const handleMenuClick = (e: any) => {
+    setCurrent(e.key);
+  }
+
   return (
     <div className="sidebar" style={{height: '100%'}}>
       <Menu
-        selectedKeys={["dashboard"]}
-        defaultSelectedKeys={["dashboard"]}
+        selectedKeys={[current]}
+        defaultSelectedKeys={[current]}
         style={{ width: 256, height: '100%' }}
+        onClick={handleMenuClick}
       >
         <Title
           level={3}
